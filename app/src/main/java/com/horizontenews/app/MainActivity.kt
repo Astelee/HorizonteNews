c24f5a575427a5e0cb813962a410a7e55e6a8872
Diego <88157099+Astelee@users.noreply.github.com>
2026-05-11 00:27:25 -0300

    Criar app/src/main/java/com/horizontenews/app/MainActivity.kt

---
+++ app/src/main/java/com/horizontenews/app/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.horizontenews.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+interface BloggerService {
+    @GET("blogs/${Config.BLOG_ID}/posts")
+    fun getPosts(@Query("key") apiKey: String): Call<PostResponse>
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.googleapis.com/blogger/v3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(BloggerService::class.java)
+        service.getPosts(Config.API_KEY).enqueue(object : Callback<PostResponse> {
+            override fun onResponse(call: Call<PostResponse>, response: Response<PostResponse>) {
+                if (response.isSuccessful) {
+                    recyclerView.adapter = PostAdapter(response.body()?.items ?: listOf())
+                }
+            }
+            override fun onFailure(call: Call<PostResponse>, t: Throwable) {}
+        })
+    }
+}
