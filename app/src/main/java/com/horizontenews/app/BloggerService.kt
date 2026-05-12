53a82948e591f415fd470e76f1f1e974b381097d
Diego <88157099+Astelee@users.noreply.github.com>
2026-05-11 22:28:46 -0300

    Atualizar o BloggerService.kt

--- app/src/main/java/com/horizontenews/app/BloggerService.kt
+++ app/src/main/java/com/horizontenews/app/BloggerService.kt
@@ -2,11 +2,24 @@ package com.horizontenews.app
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface BloggerService {
 
-    @GET("blogs/${Config.BLOG_ID}/posts")
-    fun getPosts(@Query("key") apiKey: String): Call<PostResponse>
+    // Função para listar todas as notícias (Página Principal)
+    @GET("blogs/{blogId}/posts")
+    fun getPosts(
+        @Path("blogId") blogId: String = Config.BLOG_ID,
+        @Query("key") apiKey: String
+    ): Call<PostResponse>
+
+    // Função para pesquisar notícias (Tela de Busca)
+    @GET("blogs/{blogId}/posts/search")
+    fun searchPosts(
+        @Path("blogId") blogId: String = Config.BLOG_ID,
+        @Query("key") apiKey: String,
+        @Query("q") query: String
+    ): Call<PostResponse>
 
 }
