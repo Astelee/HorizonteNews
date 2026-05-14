8f8784e25b6da48338e6266a75deb8ced4672630
Diego <88157099+Astelee@users.noreply.github.com>
2026-05-14 09:53:21 -0300

    Atualizar o AppDatabase.kt

--- app/src/main/java/com/horizontenews/app/AppDatabase.kt
+++ app/src/main/java/com/horizontenews/app/AppDatabase.kt
@@ -1,26 +1,39 @@
 package com.horizontenews.app
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import android.content.Context
 
-@Database(entities = [SavedArticle::class], version = 1, exportSchema = false)
+@Database(
+    entities = [SavedArticle::class],
+    version = 2,
+    exportSchema = false
+)
+
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun savedArticleDao(): SavedArticleDao
-    
+
     companion object {
+
         @Volatile
         private var INSTANCE: AppDatabase? = null
-        
+
         fun getDatabase(context: Context): AppDatabase {
+
             return INSTANCE ?: synchronized(this) {
+
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "horizonte_news_db"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
                 INSTANCE = instance
+
                 instance
             }
         }
