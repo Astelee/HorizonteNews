b75bd7a88a2c39ec261f5a4d80e9f84cb41546b9
Diego <88157099+Astelee@users.noreply.github.com>
2026-05-14 09:52:42 -0300

    Atualizar o SavedArticle.kt

--- app/src/main/java/com/horizontenews/app/SavedArticle.kt
+++ app/src/main/java/com/horizontenews/app/SavedArticle.kt
@@ -5,12 +5,15 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "saved_articles")
 data class SavedArticle(
+
     @PrimaryKey
     val url: String,
+
     val title: String,
     val category: String,
     val imageUrl: String,
     val date: String,
     val content: String,
+
     val savedAt: Long = System.currentTimeMillis()
 )
