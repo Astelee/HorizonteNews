20ecb8d1e265419d4d4203799c38c07e38c01639
Diego <88157099+Astelee@users.noreply.github.com>
2026-05-14 14:16:42 -0300

    Atualizar o Configltem.kt

--- app/src/main/java/com/horizontenews/app/Configltem.kt
+++ app/src/main/java/com/horizontenews/app/Configltem.kt
@@ -1,8 +1,9 @@
 package com.horizontenews.app
 
+// Classe única para representar um item da lista de configurações
 data class ConfigItem(
     val title: String,
-    val subtitle: String,
-    val icon: Int,
+    val subtitle: String? = null,
+    val iconRes: Int? = null,
     val action: () -> Unit
 )
